a53a98491b02f2ce81d1a7061b5ae4356ec5d956
fkasnar <fk.fkasnar@gmail.com>
2024-10-06 22:09:11 +0200

    crash fix

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 @extends('layouts.app')
 
 @section('content')
@@ -26,17 +25,3 @@
         </tbody>
     </table>
 @endsection
-=======
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>IntraConnect - Home</title>
-</head>
-<body>
-    <h1>Welcome to IntraConnect</h1>
-    <p>Your go-to application for employee contact information.</p>
-</body>
-</html>
->>>>>>> 4d19dab (setting up environment)
